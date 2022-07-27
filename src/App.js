3b3b0975fdbd8c287f0012e36aeeb112ab0ba1a5
Sagar Kashyap <sagarkashyap.cc@gmail.com>
2022-07-27 23:22:31 +0530

    small tweeks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
+ 
 import LocomotiveScroll from 'locomotive-scroll';
 import Paragraph from './paragraph';
 import './App.css';
 
-gsap.registerPlugin(ScrollTrigger);
 
-const scroll = new LocomotiveScroll();
 
 
 function App() {
@@ -33,8 +31,91 @@ function App() {
         </div>
       </div>
       <Paragraph />
+      
     </div>
   );
 }
 
 export default App;
+
+document.addEventListener('readystatechange', event => { 
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll= new LocomotiveScroll({
+  el: document.querySelector(".smooth-scroll"),
+  smooth: true,
+  lerp:0.08
+});
+
+locoScroll.on("scroll",ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".smooth-scroll",{
+  scrollTop(value){
+    return arguments.length
+    ? locoScroll.scrollTop(value,0,0) : locoScroll.scroll.instance.scroll.y;
+
+  },
+  getBoundingClientRect(){
+    return{
+      top:0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+  },
+  pinType:document.querySelector(".smooth-scroll").getElementsByClassName.transform
+  ? "transform":"fixed"
+});
+
+
+const vh = (coef) => window.innerHeight * (coef / 100);
+
+const heroScroller=gsap.timeline({
+  paused:true,
+  scrollTrigger:{
+    trigger:".hero-header .h-1",
+    scroller:".smooth-scroll",
+    pin:".pin-wrapper",
+    start:"top 10%",
+    scrub: true,
+    end: `${vh(100)}`
+  }
+  
+});
+
+heroScroller.to(
+  [".hero-header.h-1",".hero-header.h-3"],
+  {
+    scale:2,
+    y:vh(150),
+    xPercent:-150
+  },
+  "heroScroll"
+)
+.to(".hero-header.h-2",{
+  scale:2,
+  y:vh(150),
+  xPercent:150
+},
+"heroScroll"
+)
+.to(
+  "#heroImage",
+  {
+    scaleY: 2.5,
+  },
+  "heroScroll"
+)
+.to(
+  "#heroImage .image",
+  {
+    scaleX: 2.5,
+    xPercent: 50,
+  },
+  "heroScroll"
+);
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+
+});
--- src/paragraph.jsx
+++ src/paragraph.jsx
@@ -6,7 +6,7 @@ function Paragraph(){
         <div className="section copy">
         <div className="section-wrapper">
           <div className="content">
-            <p>
+            <p className="text">
               It is a long established fact that a reader will be distracted by
               the readable content of a page when looking at its layout. The
               point of using Lorem Ipsum is that it has a more-or-less normal
